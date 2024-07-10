868987c444bfcad0616b022eaa5ad2b94a753c44
Aditya Viki <adityaviki01@gmail.com>
2024-07-10 14:25:34 +0530

    Rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/us/:path*",
+				destination: "https://us.davidoffgeneva.com/:path*",
+			},
+		];
+	},
+};
 
 export default nextConfig;
